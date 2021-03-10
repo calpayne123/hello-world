f9378aed415369c2b4bd51e2e715e0148744b9e1
Caleb Payne <cpayne08@email.cpcc.edu>
2021-03-09 19:00:59 -0500

    Create Project1_Caleb_Payne.cpp
    Added code for getting user input

---
+++ Project1_Caleb_Payne.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+// A program designed to determine how good your computer specs are.
+using namespace std;
+int main()
+{
+	double graphicsCardSpeed, processorSpeed, processorCores
+	int selection
+	// Identifies the inputs
+	
+	cout << "How many megahertz does your graphics card have?"; // Displays question
+	cin >> graphicsCardSpeed; // Takes input
+	
+	cout << "How many megahertz does your processor have?"; // Displays question
+	cin >> processorSpeed; // Takes input
+	
+	cout << "How many cores does your processor have?"; // Displays question
+	cin >> processorCores; // Takes input
+	
+	switch (selection)
+	{
+		case 1: {"1. 1280x720";
+			break;
+		}
+		case 2: {"2. 1920x1080";
+			break;
+		}
+		case 3: {"3. 2560x1440";
+			break;
+		}
+		case 4: {"4. 3840x2160";
+			break;
+		}
+	}
+	
+}
